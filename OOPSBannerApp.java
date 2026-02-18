e8d2934a8d5dd39c78db688187e630736ea15304
sk1208-boop <sk1208@srmist.edu.in>
2026-02-18 14:57:34 +0530

    Initial commit: UC1 Print OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,19 @@
+/**
+ * OOPSBannerApp
+ * 
+ * A simple Java application that prints "OOPS" to the console.
+ * 
+ * @author Suyash
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
+
