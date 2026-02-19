707dd7bb487608b81434d30b07858471fd28c474
sk1208-boop <sk1208@srmist.edu.in>
2026-02-19 10:05:27 +0530

    Initial commit: add HelloWorld Java file

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,8 +1,20 @@
-
+/**
+ * OOPSBannerApp
+ * 
+ * UC1: Print OOPS to Console
+ * 
+ * This program prints the text "OOPS" to the console.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
 public class OOPSBannerApp {
 
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
     public static void main(String[] args) {
         System.out.println("OOPS");
     }
 }
-
